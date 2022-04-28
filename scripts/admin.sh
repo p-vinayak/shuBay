ffc37446cd916d1fbc251dca1e81067e16e31886
Vinayak Prataap <v.prataap@setonhill.edu>
2022-04-28 14:13:58 -0400

    Fix script bug

--- scripts/admin.sh
+++ scripts/admin.sh
@@ -4,9 +4,9 @@ export FLASK_APP=main
 export FLASK_ENV=development
 export FLASK_DEBUG=1
 if [ "$1" == "revoke" ]; then
-  flask add-admin $2
-elif [ "$1" == "add" ]; then
   flask revoke-admin $2
+elif [ "$1" == "add" ]; then
+  flask add-admin $2
 else
   echo "Invalid admin command provided!"
 fi
